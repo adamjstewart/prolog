9bbc30b6e32b1b3cd92f20a00d95b699b3a85ef4
Zaid Qureshi <zaidhorse@gmail.com>
2017-12-20 13:22:39 -0600

    allowed conjunction and disjunction in ast

--- src/common.ml
+++ src/common.ml
@@ -18,18 +18,32 @@ type token =
     (* Meta-characters *)
     | EOF
 
-(* Constants *)
-type const =
-     Term of string * const list    (* atoms and compound terms *)
-   | Variable of string             (* vatiables *)     
-   | BoolConst of bool              (* booleans *)
-
-(* Expressions *)
-type exp =
-     Fact of const               (* facts ie. cat(tom). *)
-   | Rule of const * const       (* rules ie. cat(tom) :- true. *)
-
 (* Declarations *)
 type dec:
-     Clause of exp           (* for adding facts/rules to db *)
-   | Query of exp            (* for querying db *) 
+      Clause of exp           (* for adding facts/rules to db *)
+    | Query of exp            (* for querying db *) 
+
+
+(* Constants *)
+type atomic =
+      Term of string * atomic list    (* atoms and compound terms *)
+    | Variable of string             (* vatiables *)     
+    | BoolConst of bool              (* booleans *)
+    | IntConst of int        (* integers *)
+    | FloatConst of float    (* floats   *)
+    | StringConst of string  (* strings  *)
+
+(* body of a rule*)
+type pred =
+      Single of atomic               (* ie cat(X) *)
+    | Conjunction of pred * pred     (* ie parent_child(Z, X), parent_child(Z, Y) *)
+    | Disjunction of pred * pred     (* ie parent_child(Z, X); parent_child(Z, Y) *)
+(* head of a rule*)
+type head =
+      Head of atomic 
+(* Expressions: facts adn rules can become one later but this is fine *)
+type exp =
+      Fact of head               (* facts ie. cat(tom). *)
+    | Rule of head * body       (* rules ie. cat(tom) :- true. *)
+
+
