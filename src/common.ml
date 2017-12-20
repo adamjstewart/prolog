1f037199d7a7887b417adb67690383de7537fa46
Zaid Qureshi <zaidhorse@gmail.com>
2017-12-20 12:32:17 -0600

    modified ast

--- src/common.ml
+++ src/common.ml
@@ -20,17 +20,16 @@ type token =
 
 (* Constants *)
 type const =
-      AtomConst of string    (* atoms    *)
-    | IntConst of int        (* integers *)
-    | FloatConst of float    (* floats   *)
-    | StringConst of string  (* strings  *)
-    | BoolConst of bool      (* booleans *)
+     Term of string * const list    (* atoms and compound terms *)
+   | Variable of string             (* vatiables *)     
+   | BoolConst of bool              (* booleans *)
 
 (* Expressions *)
 type exp =
-      VarExp of string              (* variables                *)
-    | ConstExp of const             (* constants                *)
-    | RuleExp of exp * exp          (* Head :- Body.            *)
-    | FactExp of exp                (* Head.                    *)
-    | CompoundTermExp of exp * exp  (* functor(arg1, arg2, ...) *)
-    | ListExp of exp * exp          (* arg1, arg2               *)
+     Fact of const               (* facts ie. cat(tom). *)
+   | Rule of const * const       (* rules ie. cat(tom) :- true. *)
+
+(* Declarations *)
+type dec:
+     Clause of exp           (* for adding facts/rules to db *)
+   | Query of exp            (* for querying db *) 
