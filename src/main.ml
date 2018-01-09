2cc280c492493120ab78b0ccc60f4bb03e690b5f
Adam J. Stewart <ajstewart426@gmail.com>
2018-01-09 15:53:29 -0600

    Reformat main

--- src/main.ml
+++ src/main.ml
@@ -9,32 +9,39 @@ open Evaluator
 let is_interactive = 0 = (Sys.command "[ -t 0 ]")
 
 let _ =
-  (if is_interactive
-      then print_endline "\nWelcome to the Prolog Interpreter \n"
-      else ());
-  let rec loop db =
-  try
-    let lexbuf = Lexing.from_channel stdin
-    in (if is_interactive 
-          then (print_string "> "; flush stdout)
-          else ());
-       (try
-         
-          let dec = clause (fun lb -> match Lexer.token lb with 
-                                    | EOF -> raise Lexer.EndInput
-				    | r -> r)
-                    lexbuf 
-          in 
-             let newdb = eval_dec (dec,db) in loop newdb
-            
-        with Failure s -> (print_newline();
-			   print_endline s;
-                           print_newline();
-                           loop db)
-           | Parser.Error ->
-             (print_string "\ndoes not parse\n";
-              loop db)
-           );
-       
-  with Lexer.EndInput -> exit 0
- in (loop [Clause (TermExp ("true", []), [ConstExp (BoolConst true)])] )
+    (if is_interactive
+     then print_endline "\nWelcome to the Prolog Interpreter\n"
+     else ());
+    let rec loop db = (
+        try
+            let lexbuf = Lexing.from_channel stdin
+            in (
+                if is_interactive
+                then (print_string "> "; flush stdout)
+                else ()
+            ); (
+                try
+                    let dec = clause (
+                        fun lb -> (
+                            match Lexer.token lb with
+                            | EOF -> raise Lexer.EndInput
+			    | r -> r
+			)
+		    ) lexbuf
+                    in let newdb = eval_dec (dec,db)
+                    in loop newdb
+                with
+                | Failure s -> (
+                    print_newline();
+		    print_endline s;
+                    print_newline();
+                    loop db
+                )
+                | Parser.Error -> (
+                    print_string "\ndoes not parse\n";
+                    loop db
+                )
+            );
+        with Lexer.EndInput -> exit 0
+    )
+    in (loop [Clause (TermExp ("true", []), [ConstExp (BoolConst true)])])
